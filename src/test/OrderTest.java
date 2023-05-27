7ae14a4f9f3b3fad67be4e428078f8db83a7d9f5
Kim sungsu <ksungsu>
2023-05-28 01:03:55 +0900

    commit git diff test2

--- src/test/OrderTest.java
+++ src/test/OrderTest.java
@@ -8,7 +8,7 @@ import main.ProductMenu;
  */
 public class OrderTest {
     public static void main(String[] args) {
-        // 임의로 값 넣기
+        // 임의로 값 넣기, git diif 테스트
         ProductMenu productMenu = new ProductMenu("버거", 5.5, "양파,불고기,겨자소스가 들어갔습니다");
         OrderList orderList = new OrderList();
 
